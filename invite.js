688c2b8bfdfb2278ce2609dc8a730eba52101708
GVamp <grk.411@gmail.com>
2021-04-26 17:05:07 +0530

    Create invite.js

---
+++ invite.js
@@ -0,0 +1,28 @@
+<script>
+  
+     
+	 if(window.location.href === "https://weenigo.webflow.io/?Influ_id=Temporib"){
+		 window.location.replace("https://weenigo.webflow.io/welcomeby/temporibus-eius-libero");
+     }
+        else if(window.location.href === "https://weenigo.webflow.io/?Influ_id=Accusantiu")
+	   
+	   {
+        window.location.replace("https://weenigo.webflow.io/welcomeby/accusantium-molestiae-aut-id");
+
+}
+   else if (window.location.href === "https://weenigo.webflow.io/?Influ_id=Reiciendis")
+	   
+	   {
+        window.location.replace("https://weenigo.webflow.io/welcomeby/reiciendis");
+
+}
+  
+   else if (window.location.href === "https://weenigo.webflow.io/?Influ_id=Deserunt")
+	   
+	   {
+        window.location.replace("https://weenigo.webflow.io/welcomeby/deserunt-blanditiis");
+
+}
+
+        
+</script>
